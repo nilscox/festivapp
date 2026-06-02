e8afe0bc648fa6cf747c60079d66e31b74be042a
nils <nilscox.dev@gmail.com>
2026-06-02 19:52:15 +0200

    move icons to the src folder

--- src/app/app/[festivalId]/events/[eventId]/social-icon.tsx
+++ src/app/app/[festivalId]/events/[eventId]/social-icon.tsx
@@ -1,12 +1,12 @@
-import Bandcamp from 'app/icons/bandcamp';
-import Beatport from 'app/icons/beatport';
-import Deezer from 'app/icons/deezer';
-import Facebook from 'app/icons/facebook';
-import Instagram from 'app/icons/instagram';
-import SoundCloud from 'app/icons/soundcloud';
-import Spotify from 'app/icons/spotify';
-import YouTube from 'app/icons/youtube';
 import { GlobeIcon } from 'lucide-react';
+import Bandcamp from 'src/icons/bandcamp';
+import Beatport from 'src/icons/beatport';
+import Deezer from 'src/icons/deezer';
+import Facebook from 'src/icons/facebook';
+import Instagram from 'src/icons/instagram';
+import SoundCloud from 'src/icons/soundcloud';
+import Spotify from 'src/icons/spotify';
+import YouTube from 'src/icons/youtube';
 
 export function SocialIcon({ host, ...props }: { host: string } & React.SVGProps<SVGSVGElement>) {
   const media = host.split('.').at(-2) ?? 'unknown';
